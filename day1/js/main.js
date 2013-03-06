ddeabfd731fee789631605110e91eae2507e9352
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-06 11:00:31 -0500

    Variables, strings and numbers.

--- day1/js/main.js
+++ day1/js/main.js
@@ -1,4 +1,46 @@
-//alert("Hello, world!");
+////alert("Hello, world!");
+//
+//var message1 = "Hello world! 1";
+//var message2 = "Hello world! 2";
+//var message3 = "Hello world! 3";
+//var message4 = "Hello world! 4";
+//var message5 = "Hello world! 5";
+//var message6 = "Hello world! 6";
+//var message7 = "Hello world! 7";
+//var message8 = "Hello world! 8";
+//var message9 = "Hello world! 9";
+//var message10 = "Hello world! 10";
+////alert("I like cheesecake for breakfast");
+////alert(message8);
+//
+//var message11 = "Goodbye"; // string
+////alert(message11);
+//
+//var boxOfDonuts = 5.99; // number
+//var wantBoxes = 5;
+//var totalAmount = Math.round(boxOfDonuts * wantBoxes);
+//
+//console.log(totalAmount);
+//console.log("I need to buy doughnuts");
+//console.log("I need to collect:" + totalAmount + "dollars");
+//
+//
+//
+//
 
-var message = "Hello world!";
-alert(message);
+console.log("Once upon a time a boy that like his grandma cakes");
+var numberCakes = 1;
+console.log("One day she cooked" + numberCakes + "for a birthday party");
+console.log("The boy took without permission one of the cakes");
+console.log("Her grandma got mad with him");
+console.log("So she told him that if wanted to have a cake he had to earn it");
+console.log("He had to wash the dishes to get a cake, and for every 5 plates he washed he got a cake and a glass of milk");
+var numberOfPlates = 15;
+var valueOfCakes = 5;
+var totalAmount = numberOfPlates / valueOfCakes;
+console.log("At the end he got" + totalAmount + "cakes");
+console.log("How many plates did he washed?");
+
+
+
+
