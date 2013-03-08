79a755a42a51de0361fca310fc37d8228dc9f35d
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-08 13:21:51 -0500

    Labels

--- Expressions_Personal/js/main.js
+++ Expressions_Personal/js/main.js
@@ -1,7 +1,7 @@
-const monthlyIncome = 2000; 
+const monthlyIncome = 2000; // The monthly income is constant
 console.log("This month I had in my bank account $" + monthlyIncome + " dlls."); // I am using an expression
-const billRent = 890;
-const billInternet = 62;
-console.log("Every month I have to pay the rent $" + billRent + " and internet services for $" + billInternet);
-console.log("But this month i spent my money in different things besides my regular bills.");
+const billRent = 890; // The bill rent is constant
+const billInternet = 62; // The bill internet is constant
+console.log("Every month I have to pay the rent $" + billRent + " and internet services for $" + billInternet); // Giving information using rent and internet constants
+console.log("But this month I spent my money in different things besides my regular bills."); // String
 
