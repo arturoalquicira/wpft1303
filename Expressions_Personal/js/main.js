038f30ebda97ed4c1cbd32316ca3fc923e363e38
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-08 12:02:42 -0500

    Start Personal Expression

---
+++ Expressions_Personal/js/main.js
@@ -0,0 +1,7 @@
+const monthlyIncome = 2000;
+console.log("This month I had in my bank account $" + monthlyIncome + " dlls."); // I am using an expression
+const billRent = 890;
+const billInternet = 62;
+console.log("Every month I have to pay the rent $" + billRent + "")
+console.log("But this month i spent my money in different things besides my regular bills.");
+
---
+++ day1/day2.php
@@ -0,0 +1,9 @@
+<?php
+
+$numberOfPlates = 15;
+$valueOfCakes = 5;
+$dollarsTotal = ($numberOfPlates * $valueOfCakes);
+
+print "I spend $$dollarsTotal per month";
+
+
