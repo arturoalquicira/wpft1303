403e5356c3af96fa3f0b12908c7f24f655ff3058
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-08 14:54:22 -0500

    Fix on the first line.

--- Expressions_Personal/js/main.js
+++ Expressions_Personal/js/main.js
@@ -1,5 +1,5 @@
 const monthlyIncome = 1500; // The monthly income is constant
-console.log("This month I had in my bank account $" + monthlyIncome + " dlls."); // I am using an expression
+console.log("This month I had in my bank account $" + monthlyIncome); // I am using an expression
 const billRent = 890; // The bill rent is constant
 const billInternet = 62; // The bill internet is constant
 console.log("Every month I have to pay the rent $" + billRent + " and internet services for $" + billInternet); // Giving information using rent and internet constants
