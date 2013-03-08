1395be8c4ae8c629df067b344a8347c79e5abaff
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-08 13:34:37 -0500

    Labels and Gatorade price (variable)

--- Expressions_Personal/js/main.js
+++ Expressions_Personal/js/main.js
@@ -4,5 +4,6 @@ const billRent = 890; // The bill rent is constant
 const billInternet = 62; // The bill internet is constant
 console.log("Every month I have to pay the rent $" + billRent + " and internet services for $" + billInternet); // Giving information using rent and internet constants
 console.log("But this month I spent my money in different things besides my regular bills."); // String
-
+var priceGatorade = 15 // The price of each Gatorade case
+console.log("The first week I")
 
