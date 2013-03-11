be42b135e52f9ac6c8aaaa5cf7a9dce0bbb6f86e
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-10 21:25:51 -0400

    Add a variable (number of photos)

--- day3/Expressions_Industry/js/main.js
+++ day3/Expressions_Industry/js/main.js
@@ -1,4 +1,5 @@
 console.log("Your first job will be at Instagram."); // String
-console.log("The first task they will ask me is to fix a bunch of photographs of a bunch of users for a pilot version."); // String
-console.log("Part of this task will be ")
+console.log("The first task they will ask you is to fix a bunch of photographs of a bunch of users for a pilot version."); // String
+var numberofPhotos = 200;// Variable for number of photos to modify 
+console.log("Part of this task will be to modify the code and make some updgrades ")
 
