10cf656647cb82814bbf81afb9c042101dc5007c
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-11 07:54:08 -0400

    Start Expressions Wacky

--- Expressions_Wacky/js/main.js
+++ Expressions_Wacky/js/main.js
@@ -1,6 +1,2 @@
-alert("Please complete the survey to order a Zombie Pizza!."); // Alert message
-prompt("Choose one size (small, medium or large)"); // Ask the user for a choice
-prompt("Choose a zombie sauce (Bloody Mary Zombie, BBQ Death Girl, Black Tomato"); // Ask the user a choice
-prompt("Choose your meat (Old Man Leg, Dog Zombie Pepperoni, Italian Zombie Salami"); // Ask the user a choice
-console.log("Welcome to Zombie Pizza!"); // String
+console.log("One upon a time, there was an illness that attacked the cows from the United States, it was named \"The Crazy Cows Infection\""); // String
 
