3d7a4fd53155d00d38c03c44d2840cfecc0bc374
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-10 21:37:37 -0400

    Add a constant to an expression on line 5 and 6

--- day3/Expressions_Industry/js/main.js
+++ day3/Expressions_Industry/js/main.js
@@ -1,5 +1,8 @@
 console.log("Your first job will be at Instagram."); // String
 console.log("The first task they will ask you is to fix a bunch of photographs of a bunch of users for a pilot version."); // String
 var numberofPhotos = 200;// Variable for number of photos to modify
-console.log("Part of this task will be to modify the code and make some updgrades of " + numberofPhotos + " photos."); // Giving information with a variable
+console.log("Part of this task will be to modify the code and make some upgrades of " + numberofPhotos + " photos."); // Giving information with a variable
+const deadline = 5;
+console.log("Your supervisor told you that this task cannot take you more than " + deadline + " hours to complete"); // Add a constant to the information
+
 
