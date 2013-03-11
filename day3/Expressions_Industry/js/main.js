9c62f9e6691c1085de5cb0e9dc2b4d333ed02251
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-10 21:20:37 -0400

    String line 2.

--- day3/Expressions_Industry/js/main.js
+++ day3/Expressions_Industry/js/main.js
@@ -1,3 +1,3 @@
-console.log("My first job will be at Instagram. The first task they will ask me "); // String
-
+console.log("My first job will be at Instagram."); // String
+console.log("The first task they will ask me is to fix a bunch of photographs of a bunch of users for pilot version."); // String.
 
