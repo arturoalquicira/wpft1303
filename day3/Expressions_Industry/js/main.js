d04b62e74dc924f15021b1c7c9a8e4b66f70104d
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-10 21:30:19 -0400

    Add expressions, variables and strings to line 4

--- day3/Expressions_Industry/js/main.js
+++ day3/Expressions_Industry/js/main.js
@@ -1,5 +1,5 @@
 console.log("Your first job will be at Instagram."); // String
 console.log("The first task they will ask you is to fix a bunch of photographs of a bunch of users for a pilot version."); // String
-var numberofPhotos = 200;// Variable for number of photos to modify 
-console.log("Part of this task will be to modify the code and make some updgrades ")
+var numberofPhotos = 200;// Variable for number of photos to modify
+console.log("Part of this task will be to modify the code and make some updgrades of " + numberofPhotos + " photos."); // Giving information with a variable
 
