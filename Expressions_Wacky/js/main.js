4f20e8645ad68d513052dbee18d306e87a2e9ddb
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-11 08:29:09 -0400

    Add a constant

--- Expressions_Wacky/js/main.js
+++ Expressions_Wacky/js/main.js
@@ -1,7 +1,15 @@
 console.log("One upon a time, there was an illness that attacked the cows from the United States, it was named \"The Crazy Cows Infection\""); // String
 console.log("The gorverment took five month to figured out what was the deal with this virus and how they were going to treat the cows."); // String
 console.log("When the goverment finally got the cure for all the cows in the country, they needed to made a plan to spread the vaccine that had to be disolved in water."); // String
-console.log("So they realized that there were millions of cows around the world and the most effective plan was to take them all to the Lake Michigan to drink water with the vaccine"); //String
-var askCapacity = prompt("How many cubic miles of water do you think Lake Michigan has?"); // Ask user for a value 
-if (askCapacity > 1150)
+console.log("So they realized that there were millions of cows around the world and the most effective plan was to take them all to a big artificial lake to drink water with the vaccine"); //String
+const pi = 3.1416; // Add a constant
+var radio = 200;
+var depth = prompt("How deep enough should be an artificial lake? (Write a Number)");
+var capacityLake = (pi * radio * depth) / 3;
+console.log("And One day they asked a random guy at Full Sail that how deep enough should be an artificial lake?");
+console.log("And he said " + depth + " ft.");
+console.log("So the goverment built the artificial lake with the capacity of " + capacityLake + " miles cubic.");
+console.log("And the random guy of Full Sail received a healthy cow as a gift");
+
+
 
