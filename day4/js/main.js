60d24b926b81ec20d10ed37fb3d26ffb68198405
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-11 12:28:30 -0400

    Pushing Day 4

---
+++ day4/js/main.js
@@ -0,0 +1,15 @@
+console.log("This is my main");
+
+
+var price = 5.99;
+
+console.log("Price: " + price);
+var pizzaCount = parseInt(prompt("How many?"));
+
+console.log("Price: " + price);
+
+
+console.log("You owe me; $" + price + pizzaCount);
+
+
+
---
+++ day4/js/notes.js
@@ -0,0 +1,12 @@
+console.log("These are my notes");
+
+var phasers = parseInt(prompt("How many phasers?");
+var setting = "stun";
+
+if ((phasers > 3) || (setting == "kill=")) {
+    console.log("It's dead, Jim");
+} else {
+    console.log("Run faster than the red shirt!");
+
+}
+
