947f65e0a1d196eb15756bdebfa58be04b9d52b1
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-10 22:39:15 -0400

    Label line 8

--- Expressions_Industry/js/main.js
+++ Expressions_Industry/js/main.js
@@ -5,7 +5,7 @@ console.log("Part of this task will be to modify the code and make some upgrades
 const deadline = 6; // Add a constant for the due.
 console.log("Your supervisor told you that this task cannot take you more than " + deadline + " hours to complete"); // Add a constant to the information
 var timeperpic = 2; // Add a time variable
-const timeForLunch = prompt("How many hours you spend on having your lunch?");
+var timeForLunch = prompt("How many hours you spend on having your lunch?"); // Ask the user for a variable
 console.log("But you have realized that every photo you modify takes you 2 mins."); // String
 console.log("If you spend " + timeForLunch + " hours/minutes on having your lunch, do you think you will finish your work on time?"); // Add the value of the prompt
 var totalAmountOfTime = ((numberofPhotos * timeperpic) + timeForLunch); // Calculate the time it will take for you to finish your work
