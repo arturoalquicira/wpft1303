2e64a087bb6c6309857c63e8b38cd3186ce881e0
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-11 07:02:58 -0400

    Prompt on line 2

--- Expressions_Wacky/js/main.js
+++ Expressions_Wacky/js/main.js
@@ -1,2 +1,3 @@
-alert("Please complete the survey to order a Zombie Pizza!."); // Alert message 
+alert("Please complete the survey to order a Zombie Pizza!."); // Alert message
+prompt("Choose one size (small, medium or large)"); // Ask the user for a choice
 
