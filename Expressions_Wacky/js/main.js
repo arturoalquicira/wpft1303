acb6268f2c44b9d873742c98942aa6d0a5b57a0d
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-11 08:33:26 -0400

    Add the result of the calculation

--- Expressions_Wacky/js/main.js
+++ Expressions_Wacky/js/main.js
@@ -8,7 +8,7 @@ var depth = prompt("How deep enough should be an artificial lake? (Write a Numbe
 var capacityLake = (pi * radio * depth) / 3; // Make a calculation of the capacity for the lake
 console.log("And One day they asked a random guy at Full Sail that how deep enough should be an artificial lake?"); // String
 console.log("And he said " + depth + " ft."); // Add the value of the prompt
-console.log("So the goverment built the artificial lake with the capacity of " + capacityLake + " miles cubic.");
+console.log("So the goverment built the artificial lake with the capacity of " + capacityLake + " miles cubic."); // Add the result of the calculation
 console.log("And the random guy of Full Sail received a healthy cow as a gift");
 
 
