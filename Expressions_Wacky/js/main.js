d5569e7e4baf7e38444a89acaa7ae4ebc4540ea7
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-11 07:56:02 -0400

    String line 2

--- Expressions_Wacky/js/main.js
+++ Expressions_Wacky/js/main.js
@@ -1,2 +1,4 @@
 console.log("One upon a time, there was an illness that attacked the cows from the United States, it was named \"The Crazy Cows Infection\""); // String
+console.log("The gorverment took one month to figured out what was the deal with this virus and how they were going to treat the cows."); // String
+
 
