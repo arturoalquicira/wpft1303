99f42670046e51c4cf5c37025af6c419282b140c
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-11 08:30:24 -0400

    Add a prompt

--- Expressions_Wacky/js/main.js
+++ Expressions_Wacky/js/main.js
@@ -4,7 +4,7 @@ console.log("When the goverment finally got the cure for all the cows in the cou
 console.log("So they realized that there were millions of cows around the world and the most effective plan was to take them all to a big artificial lake to drink water with the vaccine"); //String
 const pi = 3.1416; // Add a constant
 var radio = 200; // Add a variable
-var depth = prompt("How deep enough should be an artificial lake? (Write a Number)");
+var depth = prompt("How deep enough should be an artificial lake? (Write a Number)"); // Ask the user for a value
 var capacityLake = (pi * radio * depth) / 3;
 console.log("And One day they asked a random guy at Full Sail that how deep enough should be an artificial lake?");
 console.log("And he said " + depth + " ft.");
