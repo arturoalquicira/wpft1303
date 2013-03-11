e9f8b1712cef7f605b827b4ae9ceb30dbb2b0739
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-10 21:22:59 -0400

    Fix on line 2 and 3

--- day3/Expressions_Industry/js/main.js
+++ day3/Expressions_Industry/js/main.js
@@ -1,3 +1,4 @@
-console.log("My first job will be at Instagram."); // String
-console.log("The first task they will ask me is to fix a bunch of photographs of a bunch of users for pilot version."); // String.
+console.log("Your first job will be at Instagram."); // String
+console.log("The first task they will ask me is to fix a bunch of photographs of a bunch of users for a pilot version."); // String
+console.log("Part of this task will be ")
 
