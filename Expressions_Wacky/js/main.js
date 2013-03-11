db9b095b9e5379a3f52f8df336112f43c50389c0
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-11 08:06:16 -0400

    Prompt on line 5

--- Expressions_Wacky/js/main.js
+++ Expressions_Wacky/js/main.js
@@ -2,5 +2,6 @@ console.log("One upon a time, there was an illness that attacked the cows from t
 console.log("The gorverment took five month to figured out what was the deal with this virus and how they were going to treat the cows."); // String
 console.log("When the goverment finally got the cure for all the cows in the country, they needed to made a plan to spread the vaccine that had to be disolved in water."); // String
 console.log("So they realized that there were millions of cows around the world and the most effective plan was to take them all to the Lake Michigan to drink water with the vaccine"); //String
-
+var askCapacity = prompt("How many cubic miles of water do you think Lake Michigan has?"); // Ask user for a value 
+if (askCapacity > 1150)
 
