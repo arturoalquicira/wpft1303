e5de9058741982a4b4a6692d87f23fb120fe21f1
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-11 08:02:04 -0400

    String on line 4

--- Expressions_Wacky/js/main.js
+++ Expressions_Wacky/js/main.js
@@ -1,5 +1,6 @@
 console.log("One upon a time, there was an illness that attacked the cows from the United States, it was named \"The Crazy Cows Infection\""); // String
 console.log("The gorverment took five month to figured out what was the deal with this virus and how they were going to treat the cows."); // String
 console.log("When the goverment finally got the cure for all the cows in the country, they needed to made a plan to spread the vaccine that had to be disolved in water."); // String
+console.log("So they realized that there were millions of cows around the world and the most effective plan was to take them all to the Lake Michigan to drink water with the vaccine"); //String
 
 
