ddfdad92edb5a20f1f31a8b48b6ffb99744ea6b3
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-13 11:32:14 -0400

    Loops

---
+++ day5/js/loops.js
@@ -0,0 +1,18 @@
+console.log("We are in loops.js");
+
+var number = Math.round(10 * number);
+console.log("Number: " + number);
+
+var guess;
+var isCorrect = false;
+
+while ( !isCorrect) {
+    guess = parseInt(prompt("What is your guess?"));
+    console.log("Your guess: " + guess);
+    isCorrect = (guess == number);
+    if (!isCorrect) {
+        alert("Wrong!");
+
+    }
+}
+
