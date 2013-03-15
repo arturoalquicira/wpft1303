4b90ebee587414d12add51f2ad1f36e09c48c456
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-15 10:44:07 -0400

    Problem 3 adjust

--- day6/euler.js
+++ day6/euler.js
@@ -3,36 +3,25 @@
 
 // Find factors
 // Factors are numbers that go into a larger number evenly
-var factors = [];
+var biggestPrime = 1;
 var bigNumber = 600851475143;
 var currentNumber = 1;
 while (currentNumber < Math.sqrt(bigNumber)) {
     // Factors are numbers that go into a larger number evenly
     if ((bigNumber % currentNumber) == 0) {
-        factors.push(currentNumber);
-    }
-    currentNumber += 1;
-}
-console.log(factors);
+       console.log("Factor: " + currentNumber);
 // Figure out which factors are prime
-// Prime numbers are only divisible by 1 and that number
-var primeFactors = [];
-for (var arrayIndex = 0 ; arrayIndex < factors.length ; arrayIndex++) {
-    // For each item in my array...
-    var factor = factors[arrayIndex];
     var isPrime = true;
-    for (var divisor = 2 ; divisor < Math.sqrt(factor) && isPrime ; divisor++ ) {
-        if ((factor % divisor) == 0) {
+    for (var divisor = 2 ; divisor < Math.sqrt(biggestPrime) && isPrime ; divisor++ ) {
+        if ((currentNumber % divisor) == 0) {
             isPrime = false;
         } // if is a factor
     } // for divisor
     if (isPrime) {
-        primeFactors.push(factor);
+        biggestPrime = currentNumber;
     } // if isPrime
 } // for arrayIndex
-console.log(primeFactors);
-
-// Find the largest prime
-var biggestPrime = primeFactors.pop();
+currentNumber += 1;
+}
 console.log("The largest prime factor of " + bigNumber + " is " + biggestPrime);
 
