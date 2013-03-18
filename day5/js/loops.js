dbce8ffcd99a36111b1b87f913badf4dea462b99
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-18 09:10:15 -0400

    update problem 5

--- day5/js/loops.js
+++ day5/js/loops.js
@@ -1,37 +1,37 @@
-console.log("We are in loops.js");
-
-
-var numbers = [];
-var nextNumber = 1;
-
-
-while (nextNumber >0) {
-    nextNumber = parseInt(prompt("Enter a number, or blank to stop."));
-    if (!isNaN(nextNumber)) {
-        numbers.push(nextNumber);
-    }
-
-}
-
-console.log(numbers);
-var smallestNumber = numbers[0];
-var largestNumber = numbers[1];
-for (var total = 0, i=0; i< numbers.length; i=i+1) {
-    total += numbers[i];
-    if (numbers[i] < smallestNumber) {
-        smallestNumber = numbers[i];
-    }
-    if (numbers[i] > largestNumber) {
-        largestNumber = numbers[i];
-    }
-
-
-}
-console.log("Total: " + total);
-console.log("Smallest: " + smallestNumber);
-console.log("Largest: " + largestNumber);
-
-
+//console.log("We are in loops.js");
+//
+//
+//var numbers = [];
+//var nextNumber = 1;
+//
+//
+//while (nextNumber >0) {
+//    nextNumber = parseInt(prompt("Enter a number, or blank to stop."));
+//    if (!isNaN(nextNumber)) {
+//        numbers.push(nextNumber);
+//    }
+//
+//}
+//
+//console.log(numbers);
+//var smallestNumber = numbers[0];
+//var largestNumber = numbers[1];
+//for (var total = 0, i=0; i< numbers.length; i=i+1) {
+//    total += numbers[i];
+//    if (numbers[i] < smallestNumber) {
+//        smallestNumber = numbers[i];
+//    }
+//    if (numbers[i] > largestNumber) {
+//        largestNumber = numbers[i];
+//    }
+//
+//
+//}
+//console.log("Total: " + total);
+//console.log("Smallest: " + smallestNumber);
+//console.log("Largest: " + largestNumber);
+//
+//
 //var numbers = [];
 //var nextNumber = 1;
 //
@@ -54,14 +54,14 @@ console.log("Largest: " + largestNumber);
 //
 //
 //}
-// console.log("Total: " + total);
+//console.log("Total: " + total);
 //console.log("Smallest: " + smallestNumber);
-
-
-
+//
+//
+//
 //var guess;
 //var isCorrect = false;
-
+//
 //for ( var isCorrect = false, guess ; !isCorrect ; )
 //{ // 1st: initializer (set up) ; 2nd condition ; 3rd: what changes?
 //    guess = parseInt(prompt("What is your guess?"));
@@ -71,7 +71,7 @@ console.log("Largest: " + largestNumber);
 //        alert("Wrong!");
 //
 //}
-
+//
 //while ( !isCorrect) {
 //    guess = parseInt(prompt("What is your guess?"));
 //    console.log("Your guess: " + guess);
---
+++ day6/problem5.js
@@ -0,0 +1,19 @@
+var i = 1;
+number = true;
+
+
+while (number == true){
+      if (i % 20 == 0 &&
+          i % 19 == 0
+
+          )
+      {
+          notDivisible = false;
+
+} else {
+          i++;
+      }
+}
+
+console.log("" + i);
+
