2246255bb9a5c9616e2516ca2a8e6937199bf50d
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-24 22:27:49 -0400

    comment line 2 and 3

--- WPF1303/Functions_Personal/personal.js
+++ WPF1303/Functions_Personal/personal.js
@@ -1,3 +1,3 @@
 var time = prompt("How many hours do you think Arturo rides his bike?"); // ask the user for a value
 console.log("Arturo has been riding his bike last week to make some exercise. " +
-"If he reaches out an average speed of 10 m/h, and last week he rode his bike for " + time + " hours."); 
+"If he reaches out an average speed of 10 m/h, and last week he rode his bike for " + time + " hours."); // expression 
