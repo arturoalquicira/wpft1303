f7ddcb6ba49a6602348b9b48547a8ca0f2e1460e
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-24 22:43:21 -0400

    comment line 10

--- WPF1303/Functions_Personal/personal.js
+++ WPF1303/Functions_Personal/personal.js
@@ -7,4 +7,4 @@ var distance = function( time, speed) { // function with two parameters
     if ( answer >= 15 ) { // if statement
         console.log("He is working out!"); // print out a string
     } else { // add an else statement
-        console.log("He has been such a lazy guy!");
+        console.log("He has been such a lazy guy!"); // print out the else statement 
