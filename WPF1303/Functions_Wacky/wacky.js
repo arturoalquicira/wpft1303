669effb4aa7715f352d2b5694ce9001c7ae2e41b
arturoalquicira <arturoalquicira@fullsail.edu>
2013-03-27 17:04:47 -0400

    make a variable and a loop

--- WPF1303/Functions_Wacky/wacky.js
+++ WPF1303/Functions_Wacky/wacky.js
@@ -2,27 +2,33 @@ var tennis = [
     {
         "model" : "vans",
         "color" : "black",
-        "size"  : "7.5"
+        "size"  : 7.5,
+        "price" : 27
     },
     {
         "model" : "nike",
         "color" : "red",
-        "size"  : "9.5"
+        "size"  : 9.5,
+        "price" : 35
     },
     {
         "model" : "adidas",
         "color" : "blue",
-        "size"  : "10"
+        "size"  : 10,
+        "price" : 30
     },
     {
         "model" : "rebook",
         "color" : "red",
-        "size"  : "9.5"
+        "size"  : 9.5,
+        "price" : 24
     }
 ];
 
 var newTennis = function(tennis){
     for (var tennisNumber = 0; tennisNumber < tennis.length; tennisNumber++){
+        var modelOfTennis = tennis[tennisNumber];
+        if
 
     }
 }
